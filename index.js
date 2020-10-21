be9f2c7436349a485a4dda88d11aac6d0f240356
Izzy Bolt <izzy@VIV-ID-MBA-04.local>
2020-10-21 10:22:08 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+// npm i express
+const app = express();
+// initialised express function to use it throughout the file
+
+// PORT = process.env.PORT
+
+const routes = require('./routes');
+// set up our routes so our app.gets(s) are in another file,
+// keeping our index.js nice and clean
+
+app.use('/', routes);
+
+app.listen(process.env.PORT || 3005, () => {
+    console.log('Server is listening.')
+})
+
